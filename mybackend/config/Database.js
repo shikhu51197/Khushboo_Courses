9d16ef6a5c2bd299b85a4e5916b0e893bcae32d1
Shikha Gupta <shikhagupta@192.168.1.2>
2026-06-25 03:41:07 +0530

    Fix: Add local MongoDB fallback URL

--- mybackend/config/Database.js
+++ mybackend/config/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-   const {connection} = await mongoose.connect(process.env.MONGO_URI)
+   const {connection} = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/Learnify")
 
    console.log(`Mongodb is connected with ${connection.host}`)
 }
